ef5a84e15a2579a1658e37e74fa0525d60f3ea1e
amaryfilo <laumiqv@gmail.com>
2025-08-27 20:25:38 +0200

    vercel scripts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { isDevMode } from '@angular/core';
 import { App } from './app/app';
+import { appConfig } from './app/app.config';
+
+import { inject as injectAnalytics } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 bootstrapApplication(App, appConfig)
+  .then(() => {
+    const isBrowser =
+      typeof window !== 'undefined' && typeof document !== 'undefined';
+    if (isBrowser && !isDevMode()) {
+      injectAnalytics();
+      injectSpeedInsights();
+    }
+  })
   .catch((err) => console.error(err));
