a13812818038bf1bce3180b994fe106cd35dd65e
amaryfilo <laumiqv@gmail.com>
2025-09-12 21:26:41 +0200

    marketplace page

--- src/app/features/sandbox/projects/crypto/wallet-tools/page/components/marketplace/marketplace.component.ts
+++ src/app/features/sandbox/projects/crypto/wallet-tools/page/components/marketplace/marketplace.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BalancesService } from '../../../services/balances.service';
+import { WalletFacade } from '../../../services/wallet-facade.service';
+import { UIAccordionComponent } from '@sandbox/shared/components/accordion/accordion.component';
+import { UIButtonComponent } from '@sandbox/shared/components/button/button.component';
 
 @Component({
   selector: 'sbx-wallet-tools-marketplace-tab',
-  imports: [],
+  imports: [UIAccordionComponent, UIButtonComponent],
   templateUrl: './marketplace.component.html',
   styleUrl: './marketplace.component.scss',
   standalone: true,
 })
-export class WalletToolsMarketplaceComponent {}
+export class WalletToolsMarketplaceComponent {
+  private facade = inject(WalletFacade);
+  private balances = inject(BalancesService);
+
+  readonly status = this.facade.status;
+}
