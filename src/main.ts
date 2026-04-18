a7973e51e80f8049e8db8869dca57e4c43b70b04
amaryfilo <laumiqv@gmail.com>
2026-04-19 01:04:30 +0200

    vercel packages

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 // src/main.ts
 
 import { bootstrapApplication } from '@angular/platform-browser';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+
+injectSpeedInsights();
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
